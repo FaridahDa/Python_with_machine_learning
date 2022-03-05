63197fd3ded11b5fecefe477bd5c701caf4a9d97
Faridah <faridahdada@outlook.com>
2022-03-05 12:43:10 +0000

    added exercise

--- Titanic_model_dataset/TitanicModel.py
+++ Titanic_model_dataset/TitanicModel.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
 
-titanic_dataset = pd.read_csv()
+titanic_dataset = pd.read_csv('train.csv')
+
+print(titanic_dataset)
 
 
+def check_login():
+    user_details = {"username": "Faridah", "password": "faridah123"}
+    username = input(" Please enter your username:")
+    password = input("please enter your password:")
+    if username == user_details["username"] and password == user_details["password"]:
+        print(f"Welcome {username}")
+    else:
+        print("welcome")
 
 
+check_login()
